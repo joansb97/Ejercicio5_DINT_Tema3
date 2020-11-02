e7d58070d930cb3123aceafa1aa960f6e65c3084
Joan Seva Bernabeu <joan.seva@iesdoctorbalmis.com>
2020-11-02 19:25:10 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio5_DINT_Tema3/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio5_DINT_Tema3
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(Object sender, RoutedEventArgs e)
+        {
+            resultadoTextBlock.Text = resultadoTextBlock.Text + (sender as Button).Tag.ToString();
+        }
+    }
+}
